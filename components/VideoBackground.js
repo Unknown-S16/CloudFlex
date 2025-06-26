2e5a059768b1680ec113194f27ad39bbba8e237d
Unknown-S16 <sharath20038@gmail.com>
2025-06-26 17:14:07 +0530

    UI update

--- components/VideoBackground.js
+++ components/VideoBackground.js
@@ -1,14 +1,17 @@
-// components/VideoBackground.js
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function VideoBackground() {
   const [hoverPos, setHoverPos] = useState({ x: 0, y: 0 });
   const [isHovering, setIsHovering] = useState(false);
 
+  const fallbackPos = { x: 200, y: 400 }; 
+
+  const currentPos = isHovering ? hoverPos : fallbackPos;
+
   return (
     <div
-      className="fixed inset-0 z-4 overflow-hidden"
+      className="fixed inset-0 z-[-1] overflow-hidden"
       onMouseMove={(e) => {
         setIsHovering(true);
         setHoverPos({ x: e.clientX, y: e.clientY });
@@ -20,18 +23,14 @@ export default function VideoBackground() {
         autoPlay
         muted
         loop
-        className="absolute w-full h-full object-cover brightness-200 contrast-400 hue-rotate-400 "
+        className="absolute w-full h-full object-cover brightness-200 contrast-400 hue-rotate-400"
       />
 
       <div
-        className="absolute inset-0 pointer-events-none"
+        className="absolute inset-0 pointer-events-none transition-all duration-300"
         style={{
-          WebkitMaskImage: isHovering
-            ? `radial-gradient(circle 200px at ${hoverPos.x}px ${hoverPos.y}px, transparent 0%, black 100%)`
-            : "none",
-          maskImage: isHovering
-            ? `radial-gradient(circle 300px at ${hoverPos.x}px ${hoverPos.y}px, transparent 0%, black 100%)`
-            : "none",
+          WebkitMaskImage: `radial-gradient(circle 250px at ${currentPos.x}px ${currentPos.y}px, transparent 0%, black 100%)`,
+          maskImage: `radial-gradient(circle 250px at ${currentPos.x}px ${currentPos.y}px, transparent 0%, black 100%)`,
           backgroundColor: "rgba(0, 0, 0, 1)",
         }}
       />
